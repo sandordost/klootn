fe69e6723932d032500f20d9584626b569a05ca0
sandordost <sandor1998@hotmail.com>
2023-01-03 17:31:16 +0100

    BugFix

--- Assets/Scripts/Managers/FirestoreManager.cs
+++ Assets/Scripts/Managers/FirestoreManager.cs
@@ -327,7 +327,7 @@ public class FirestoreManager : IDatabaseManager
 	{
 		CollectionReference lobbiesRef = firestore.Collection("Lobbies");
 
-		await lobbiesRef.Document($"{lobbyId}.PlayersLastSeen.{playerId}").DeleteAsync();
+		await lobbiesRef.Document($"{lobbyId}.LobbyPlayersData.{playerId}").DeleteAsync();
 	}
 
 	public async Task UpdateLobbyMap(string lobbyId, string mapId)
